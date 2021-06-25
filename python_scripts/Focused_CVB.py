92c7bd0f787ecea56cabf7cf76d1521be53941d0
Gonzague Agez <agez@cemes.fr>
2021-06-25 18:10:49 +0200

    with examples

--- python_scripts/Focused_CVB.py
+++ python_scripts/Focused_CVB.py
@@ -4,13 +4,20 @@
 """
 Created on Wed Apr 15 00:14:19 2020
 
-@author: agez
+@author: Gonzague Agez, Martin Montagnac, Arnaud Arbouet, Vincent Paillard
+@Affiliation : CEMES - CNRS (France)
 
-M2 : test autour du M2 facteur de qualité
-v1.0 : avec import PVB_source
-v1.1 : mise en forme des parser
-v1.2 : import source MA + Nov
+This script proposes two methods to implement Focused Cylindrical Vector Beams (Azimuthal and Radial) in meep
+
+Laguerre-Gaussian Method :
+    - rapid
+    - for NA < 0.9 with suitable M² quality factor
+Novotny-Hetch Method :
+    - time consuming
+    - no limits for NA (with suitable index medium)  
+
 """
+
 import PVB_Source as pvb
 import meep as mp
 import numpy as np
@@ -25,23 +32,25 @@ start_time = time.time()
 
 parser = argparse.ArgumentParser()  
 # General
-parser.add_argument('-resolution',  type=int,     default = 20,   help='resolution')
-parser.add_argument('-sxy',         type=float,   default = 7,    help='transverse size of the cell (without PML)')
-parser.add_argument('-sz',          type=float,   default = 2,    help='longitudinal size of the cell (without PML)')
-parser.add_argument('-dpml',        type=float,   default = 0.9,  help='PML thickness')
-parser.add_argument('-runtime',     type=float,   default = 10,   help='run until')
+parser.add_argument('-resolution',  type=int,     default = 40,   help='resolution (points by µm)')
+parser.add_argument('-sxy',         type=float,   default = 4,    help='in µm, transverse size of the cell (without PML), must be large for NH method')
+parser.add_argument('-sz',          type=float,   default = 4,    help='in µm, longitudinal size of the cell (without PML)')
+parser.add_argument('-dpml',        type=float,   default = 0.5,  help='PML thickness')
+parser.add_argument('-runtime',     type=float,   default = 10,   help='in meep time unit, run until')
 
-parser.add_argument('-beam',        type=str,     default ='rad_ma', help='type de faisceau : plane,gauss,azi_ma,rad_ma or azi_nov rad_nov')
-parser.add_argument('-wvl',         type=float,   default = 500,  help='en nm, longueur d onde')
-parser.add_argument('-NA',          type=float,   default = 0.5 , help='Numerical aperture')
+parser.add_argument('-method',      type=str,     default ='LG',     help='method : LG for Laguerre-Gauss or NH for Novotny-Hetch')
+parser.add_argument('-beam',        type=str,     default ='gauss',  help='beam type : azi, rad, gauss')
+parser.add_argument('-wvl',         type=float,   default = 633,     help='in nm, wavelength')
+parser.add_argument('-NA',          type=float,   default = 0.3 ,    help='Numerical aperture')
 
-# For Paraxial Laguerre-Gauss method
-parser.add_argument('-M2',          type=float,   default = 1.5 ,   help='M² quality factor (only for MA method)')
+# Only For Paraxial Laguerre-Gauss method
+parser.add_argument('-M2',          type=float,   default = 1 ,    help='M² quality factor (only for LG method, = 1 for small NA )')
+# see the ref to choose M2 suitable value : Mei, Z., Zhao, D., & Zheng, C. (2005). Analytical expressions of generalized M2 factor of hard-edged diffracted Laguerre–Gaussian beams. Optik, 116(7), 337-342.
 
-# For Non-Paraxial Novotny method
-parser.add_argument('-w0',          type=float,   default = 1000 ,help='in um, beam waist before aplanatic lens (only for Novotny method)')
-parser.add_argument('-f',           type=float,   default = 1000 ,help='in um, focal length of the aplanatic lens (only for Novotny method)')
-parser.add_argument('-n',           type=float,   default = 1,    help='refractive index of the source medium')
+# Only For Non-Paraxial Novotny-Hecht method
+parser.add_argument('-w0',          type=float,   default = 1000 ,  help='in um, beam waist before aplanatic lens (only for Novotny method)')
+parser.add_argument('-f',           type=float,   default = 1000 ,  help='in um, focal length of the aplanatic lens (only for Novotny method)')
+parser.add_argument('-n',           type=float,   default = 1,      help='refractive index of the source medium')
 
 args = parser.parse_args()
 
@@ -57,11 +66,11 @@ runtime     = args.runtime
 beam        = args.beam
 wvl         = args.wvl/1000
 NA          = args.NA 
-
-# For Paraxial Laguerre-Gauss method
+method      = args.method
+# M² Quality factor for Paraxial Laguerre-Gauss method
 M2          = args.M2     
 
-# For Non-Paraxial Novotny method
+# For Non-Paraxial Novotny-Hetch method (Novotny, L., & Hecht, B. (2012). Principles of nano-optics. Cambridge university press.)
 w0          = args.w0
 f           = args.f   
 n           = args.n 
@@ -72,7 +81,10 @@ k           = 2*n*np.pi*fcen            # source central wavenumber
 waist       = wvl/(np.pi*NA)            # waist at focal point (MA method)
 
 # Output Name
-beamtxt     = beam+'_M2='+str(M2)+'_Sxy='+str(sx)+'_Sz='+str(sz)+'_reso='+str(resolution)+'_Wvl='+str(wvl)+'_NA='+str(NA)+'_t='+str(runtime)
+if method == 'LG':
+    beamtxt     = beam+method+'_M2='+str(M2)+'_Sxy='+str(sx)+'_Sz='+str(sz)+'_reso='+str(resolution)+'_Wvl='+str(wvl)+'_NA='+str(NA)+'_t='+str(runtime)
+if method == 'NH' :
+    beamtxt     = beam+method+'_w0='+str(w0)+'_f='+str(f)+'_Sxy='+str(sx)+'_Sz='+str(sz)+'_reso='+str(resolution)+'_Wvl='+str(wvl)+'_NA='+str(NA)+'_t='+str(runtime)
 
 #%%% CELL PROPERTIES %%%
 
@@ -81,74 +93,79 @@ cell_y=sy+2*dpml
 cell_z=sz+2*dpml
 cell_size = mp.Vector3(cell_x,cell_y,cell_z)
 
-pml_layers = [mp.Absorber(dpml)]   
+pml_layers = [mp.Absorber(dpml)]
 
+medium = mp.Medium(epsilon = n*n)  
+
+geometry = [ mp.Block(size     = cell_size, 
+                      center   = mp.Vector3(), 
+                      material = medium)]
 
 #%%% SOURCE PROPERTIES %%%
 center_x = 0
 center_y = 0
 center_z = - sz/2
-source_size = mp.Vector3(cell_x  ,cell_y  ,0.0)
+source_size = mp.Vector3(cell_x  ,cell_y  ,0.0     )
 source_pos  = mp.Vector3(center_x,center_y,center_z)
 
-Source_Gauss_MA = [mp.Source(mp.ContinuousSource(frequency=fcen,is_integrated = True),
+Source_Gauss_LG = [mp.Source(mp.ContinuousSource(frequency=fcen,is_integrated = True),
                              center = source_pos,
                              size = source_size,
                              component = mp.Ex,
                              amp_func = lambda r: pvb.LG(r.x,r.y,center_z,0,fcen,waist,M2))]
  
 
-Source_Azi_MA =   [mp.Source(mp.ContinuousSource(frequency=fcen,is_integrated = True),
+Source_Azi_LG =   [mp.Source(mp.ContinuousSource(frequency=fcen,is_integrated = True),
                              center = source_pos,
                              size = source_size,
                              component = mp.Ex,
-                             amp_func = lambda r: pvb.PVB_ma_x(r.x,r.y,center_z,fcen,waist,M2)),
+                             amp_func = lambda r: pvb.PVB_LG_x(r.x,r.y,center_z,fcen,waist,M2)),
               
                    mp.Source(mp.ContinuousSource(frequency=fcen,is_integrated = True),
                              center = source_pos,
                              size = source_size,
                              component = mp.Ey,
-                             amp_func = lambda r: pvb.PVB_ma_y(r.x,r.y,center_z,fcen,waist,M2))]
+                             amp_func = lambda r: pvb.PVB_LG_y(r.x,r.y,center_z,fcen,waist,M2))]
 
-Source_Rad_MA =   [mp.Source(mp.ContinuousSource(frequency=fcen,is_integrated = True),
+Source_Rad_LG =   [mp.Source(mp.ContinuousSource(frequency=fcen,is_integrated = True),
                              center = source_pos,
                              size = source_size,
                              component = mp.Hx,
-                             amp_func = lambda r: pvb.PVB_ma_x(r.x,r.y,center_z,fcen,waist,M2)),
+                             amp_func = lambda r: pvb.PVB_LG_x(r.x,r.y,center_z,fcen,waist,M2)),
               
                    mp.Source(mp.ContinuousSource(frequency=fcen,is_integrated = True),
                              center = source_pos,
                              size = source_size,
                              component = mp.Hy,
-                             amp_func = lambda r: pvb.PVB_ma_y(r.x,r.y,center_z,fcen,waist,M2))]
+                             amp_func = lambda r: pvb.PVB_LG_y(r.x,r.y,center_z,fcen,waist,M2))]
 
-Source_Azi_Nov = [mp.Source(mp.ContinuousSource(frequency=fcen,is_integrated = True),
+Source_Azi_NH = [mp.Source(mp.ContinuousSource(frequency=fcen,is_integrated = True),
                              center = source_pos,
                              size = source_size,
                              component = mp.Ex,
-                             amp_func = lambda r: pvb.PVB_nov_x(r.x,r.y,center_z,k,n,NA,f,w0)),
+                             amp_func = lambda r: pvb.PVB_NH_x(r.x,r.y,center_z,k,n,NA,f,w0)),
                   
                   mp.Source(mp.ContinuousSource(frequency=fcen,is_integrated = True),
                              center = source_pos,
                              size = source_size,
                              component = mp.Ey,
-                             amp_func = lambda r: pvb.PVB_nov_y(r.x,r.y,center_z,k,n,NA,f,w0))]
+                             amp_func = lambda r: pvb.PVB_NH_y(r.x,r.y,center_z,k,n,NA,f,w0))]
 
-Source_Rad_Nov = [mp.Source(mp.ContinuousSource(frequency=fcen,is_integrated = True),
+Source_Rad_NH = [mp.Source(mp.ContinuousSource(frequency=fcen,is_integrated = True),
                              center = source_pos,
                              size = source_size,
                              component = mp.Hx,
                              amplitude = -1,
-                             amp_func = lambda r: pvb.PVB_nov_x(r.x,r.y,center_z,k,n,NA,f,w0)),
+                             amp_func = lambda r: pvb.PVB_NH_x(r.x,r.y,center_z,k,n,NA,f,w0)),
                   
                   mp.Source(mp.ContinuousSource(frequency=fcen,is_integrated = True),
                              center = source_pos,
                              size = source_size,
                              component = mp.Hy,
                              amplitude = -1,
-                             amp_func = lambda r: pvb.PVB_nov_y(r.x,r.y,center_z,k,n,NA,f,w0))]
+                             amp_func = lambda r: pvb.PVB_NH_y(r.x,r.y,center_z,k,n,NA,f,w0))]
 
-Source_Gauss_Nov = [mp.Source(mp.ContinuousSource(frequency=fcen,is_integrated = True),
+Source_Gauss_NH = [mp.Source(mp.ContinuousSource(frequency=fcen,is_integrated = True),
                              center = source_pos,
                              size = source_size,
                              component = mp.Ex,
@@ -165,31 +182,38 @@ Source_Gauss_Nov = [mp.Source(mp.ContinuousSource(frequency=fcen,is_integrated =
                              component = mp.Ez,
                              amp_func = lambda r: pvb.Ez_H00(r.x,r.y,center_z,k,n,NA,f,w0))]
 
-if beam == 'gauss_ma' :
-    Source = Source_Gauss_MA
-    symmetries = [mp.Mirror(mp.Y,+1),mp.Mirror(mp.X,-1)]
-if beam == 'azi_ma' :
-    Source = Source_Azi_MA
-    symmetries = [mp.Mirror(mp.Y,-1),mp.Mirror(mp.X,-1)]
-if beam == 'rad_ma' :
-    Source = Source_Rad_MA   
-    symmetries = [mp.Mirror(mp.Y,+1),mp.Mirror(mp.X,+1)]
-if beam == 'gauss_nov' :
-    Source = Source_Gauss_Nov
-    symmetries = [mp.Mirror(mp.Y,+1),mp.Mirror(mp.X,-1)]    
-if beam == 'azi_nov' :
-    Source = Source_Azi_Nov
-    symmetries = [mp.Mirror(mp.Y,-1),mp.Mirror(mp.X,-1)]
-if beam == 'rad_nov' :
-    Source = Source_Rad_Nov 
-    symmetries = [mp.Mirror(mp.Y,+1),mp.Mirror(mp.X,+1)]
+if method == 'LG':
+    if beam == 'gauss' :
+        Source = Source_Gauss_LG
+        symmetries = [mp.Mirror(mp.Y,+1),mp.Mirror(mp.X,-1)]
+    if beam == 'azi' :
+        Source = Source_Azi_LG
+        symmetries = [mp.Mirror(mp.Y,-1),mp.Mirror(mp.X,-1)]
+    if beam == 'rad' :
+        Source = Source_Rad_LG   
+        symmetries = [mp.Mirror(mp.Y,+1),mp.Mirror(mp.X,+1)]
+
+if method == 'NH':
+    if beam == 'gauss' :
+        Source = Source_Gauss_NH
+        symmetries = [mp.Mirror(mp.Y,+1),mp.Mirror(mp.X,-1)]    
+    if beam == 'azi' :
+        Source = Source_Azi_NH
+        symmetries = [mp.Mirror(mp.Y,-1),mp.Mirror(mp.X,-1)]
+    if beam == 'rad' :
+        Source = Source_Rad_NH 
+        symmetries = [mp.Mirror(mp.Y,+1),mp.Mirror(mp.X,+1)]
+ 
     
+
+
+ 
 #%%% RUN %%%
 
 sim = mp.Simulation(resolution=resolution,
                     cell_size=cell_size,
                     sources=Source,
-                    # geometry=geometry,
+                    geometry=geometry,
                     symmetries=symmetries,
                     force_complex_fields = True,
                     boundary_layers = pml_layers)
@@ -199,15 +223,18 @@ sim.run(until=runtime)
 #%% DATA COLLECTION
 
     # Visualization box
-
-boxX = 4*wvl
-boxY = 4*wvl
+if NA > 0.7 :
+    boxX = 4*wvl
+    boxY = 4*wvl
+else:
+    boxX = 8*wvl
+    boxY = 8*wvl
 boxZ = sz
 box=mp.Volume(center=mp.Vector3(0,0,0),size=mp.Vector3(boxX,boxY,boxZ))
 (x,y,z,w)= sim.get_array_metadata(vol=box)
 
 
-    #Coupe longitudinale (X,Z)
+    # Longitudinal Cross Section (X,Z)
 monitor_longi=mp.Volume(center=mp.Vector3(0,0,0),size=mp.Vector3(boxX,0,boxZ))
 
 ex_longi = sim.get_array(vol=monitor_longi, component=mp.Ex)
@@ -223,7 +250,7 @@ Ie2      = np.amax(Iline)*0.135335283
 Ie2line  = np.full(len(x), Ie2)
 Hline    = abs(hx_longi[:,iz0])**2+abs(hy_longi[:,iz0])**2+abs(hz_longi[:,iz0])**2
 
-# Plan transverse transmis (X,Z)
+    # Transverse Cross Section in focal plane (X,Z)
 
 monitor_XY=mp.Volume(center=mp.Vector3(0,0,0),size=mp.Vector3(boxX,boxY,0))
 
@@ -256,9 +283,10 @@ extentXZ = [x[0], x[len(x)-1], z[0], z[len(z)-1]]
 # Plot of E²(x,y) and H²(x,y)
 
 plt.figure()
+plt.suptitle("E² and H² in focal plane")
 ax = plt.subplot(121)
 ax.set_title('E²')
-plt.imshow(np.transpose(abs(ex)**2+abs(ey)**2+abs(ez)**2),cmap='gray',extent=extentXY,origin='lower')
+plt.imshow(np.transpose(abs(ex)**2+abs(ey)**2+abs(ez)**2),cmap='gray',extent=extentXY,origin='lower',interpolation='spline16')
 ax.set_aspect('equal')
 ax.set_xlabel('x (\u03BCm)')  
 ax.set_ylabel('y (\u03BCm)') 
@@ -266,11 +294,12 @@ plt.colorbar()
 
 ax = plt.subplot(122)
 ax.set_title('H²')
-plt.imshow(np.transpose(abs(hx)**2+abs(hy)**2+abs(hz)**2),cmap='gray',extent=extentXY,origin='lower')
+plt.imshow(np.transpose(abs(hx)**2+abs(hy)**2+abs(hz)**2),cmap='gray',extent=extentXY,origin='lower',interpolation='spline16')
 ax.set_aspect('equal')
 ax.set_xlabel('x (\u03BCm)')  
 plt.colorbar()
 
+plt.tight_layout()
 plt.savefig(beamtxt+'_E2_H2_wo.png',dpi=300)
 plt.show()
 plt.close()
@@ -280,7 +309,7 @@ plt.close()
 plt.figure()
 ax = plt.subplot(221)
 ax.set_title('E² longitudinal')
-plt.imshow(np.transpose(abs(ex_longi)**2+abs(ey_longi)**2+abs(ez_longi)**2),cmap='gray',extent=extentXZ,origin='lower')
+plt.imshow(np.transpose(abs(ex_longi)**2+abs(ey_longi)**2+abs(ez_longi)**2),cmap='gray',extent=extentXZ,origin='lower',interpolation='spline16')
 ax.set_aspect('equal')
 ax.set_ylabel('z (\u03BCm)') 
 plt.colorbar()
@@ -295,7 +324,7 @@ plt.grid(True)
 
 ax = plt.subplot(222)
 ax.set_title('H² longitudinal')
-plt.imshow(np.transpose(abs(hx_longi)**2+abs(hy_longi)**2+abs(hz_longi)**2),cmap='gray',extent=extentXZ,origin='lower')
+plt.imshow(np.transpose(abs(hx_longi)**2+abs(hy_longi)**2+abs(hz_longi)**2),cmap='gray',extent=extentXZ,origin='lower',interpolation='spline16')
 ax.set_aspect('equal')
 ax.set_xlabel('x (\u03BCm)')  
 plt.colorbar()
@@ -305,6 +334,7 @@ ax.plot(x,Hline)
 ax.set_xlabel('x (\u03BCm)') 
 plt.grid(True) 
 
+plt.tight_layout()
 plt.savefig(beamtxt+'_Longitudinal.png',dpi=300)
 plt.show()
 plt.close()  
@@ -312,28 +342,29 @@ plt.close()
 # Plot of component Ex²,Ey²,Ez² and Hx², Hy³, Hz²
   
 plt.figure()
+plt.suptitle("E² and H² components in focal plane")
 ax = plt.subplot(2,3,1)
 ax.set_title('Ex²')
-plt.imshow(np.transpose(abs(ex)**2),cmap='gray',extent=extentXY,origin='lower')
+plt.imshow(np.transpose(abs(ex)**2),cmap='gray',extent=extentXY,origin='lower',interpolation='spline16')
 ax.set_aspect('equal')
 ax.set_ylabel('y (\u03BCm)') 
 plt.colorbar()
 
 ax = plt.subplot(2,3,2)
 ax.set_title('Ey²')
-plt.imshow(np.transpose(abs(ey)**2),cmap='gray',extent=extentXY,origin='lower')
+plt.imshow(np.transpose(abs(ey)**2),cmap='gray',extent=extentXY,origin='lower',interpolation='spline16')
 ax.set_aspect('equal')
 plt.colorbar()
 
 ax = plt.subplot(2,3,3)
 ax.set_title('Ez²')
-plt.imshow(np.transpose(abs(ez)**2),cmap='gray',extent=extentXY,origin='lower')
+plt.imshow(np.transpose(abs(ez)**2),cmap='gray',extent=extentXY,origin='lower',interpolation='spline16')
 ax.set_aspect('equal')
 plt.colorbar()
 
 ax = plt.subplot(2,3,4)
 ax.set_title('Hx²')
-plt.imshow(np.transpose(abs(hx)**2),cmap='gray',extent=extentXY,origin='lower')
+plt.imshow(np.transpose(abs(hx)**2),cmap='gray',extent=extentXY,origin='lower',interpolation='spline16')
 ax.set_aspect('equal')
 ax.set_ylabel('y (\u03BCm)') 
 ax.set_xlabel('x (\u03BCm)') 
@@ -341,28 +372,30 @@ plt.colorbar()
 
 ax = plt.subplot(2,3,5)
 ax.set_title('Hy²')
-plt.imshow(np.transpose(abs(hy)**2),cmap='gray',extent=extentXY,origin='lower')
+plt.imshow(np.transpose(abs(hy)**2),cmap='gray',extent=extentXY,origin='lower',interpolation='spline16')
 ax.set_aspect('equal')
 ax.set_xlabel('x (\u03BCm)') 
 plt.colorbar()
 
 ax = plt.subplot(2,3,6)
 ax.set_title('Hz²')
-plt.imshow(np.transpose(abs(hz)**2),cmap='gray',extent=extentXY,origin='lower')
+plt.imshow(np.transpose(abs(hz)**2),cmap='gray',extent=extentXY,origin='lower',interpolation='spline16')
 ax.set_aspect('equal')
 ax.set_xlabel('x (\u03BCm)') 
 plt.colorbar()
 
+plt.tight_layout()
 plt.savefig(beamtxt+'_Component.png',dpi=300)
 plt.show()
 plt.close()  
  
-# Plot of Stokes coeff
+# Plot of Stokes coeff in spherical coordinates
 
 plt.figure()
+plt.suptitle("Stokes parameters (spherical coordinates)")
 ax = plt.subplot(1,2,1)
 ax.set_title('Psi')
-plt.imshow(np.transpose(Psi),extent=extentXY,origin='lower')
+plt.imshow(np.transpose(Psi),extent=extentXY,origin='lower',interpolation='spline16')
 ax.set_aspect('equal')
 ax.set_xlabel('x (\u03BCm)')  
 ax.set_ylabel('y (\u03BCm)')  
@@ -370,16 +403,17 @@ plt.colorbar()
 
 ax = plt.subplot(1,2,2)
 ax.set_title('Chi')
-plt.imshow(np.transpose(Chi),vmin=-np.pi/4,vmax=np.pi/4,extent=extentXY,origin='lower')
+plt.imshow(np.transpose(Chi),vmin=-np.pi/4,vmax=np.pi/4,extent=extentXY,origin='lower',interpolation='spline16')
 ax.set_aspect('equal')
 ax.set_xlabel('x (\u03BCm)')  
 plt.colorbar()
 
+plt.tight_layout()
 plt.savefig(beamtxt+'_Stokes.png',dpi=300)
 plt.show()
 plt.close()
 
 
 #%% Display running time
-print("\nTemps d execution : %s secondes ---" % (time.time() - start_time))    
+print("\n Total Execution Time : %s seconds ---" % (time.time() - start_time))    
          
--- python_scripts/PVB_Source.py
+++ python_scripts/PVB_Source.py
@@ -3,14 +3,14 @@
 """
 Created on Wed Apr 14 10:39:18 2021
 
-@author: Gonzague Agez & Martin Montagnac
+@author: Gonzague Agez, Martin Montagnac, Arnaud Arbouet, Vincent Paillard
 """
 # Import
 import numpy as np
 import scipy.special as special
 from scipy import integrate
 
-# Laguerre Gauss method
+# Laguerre-Gauss method
     
 def _cart_cyl(x,y):
     rho = np.sqrt(x**2 + y**2)
@@ -44,21 +44,21 @@ def LG(x,y,z,l,fcen,waist,M2):
 
             
 
-def PVB_ma_x(x,y,z,fcen,waist,M2):
+def PVB_LG_x(x,y,z,fcen,waist,M2):
 
     return ( -1j/2*(LG(x,y,z,1,fcen,waist,M2)+LG(x,y,z,-1,fcen,waist,M2)))
 
-def PVB_ma_y(x,y,z,fcen,waist,M2):
+def PVB_LG_y(x,y,z,fcen,waist,M2):
 
     return (  1/2*(LG(x,y,z,1,fcen,waist,M2)-LG(x,y,z,-1,fcen,waist,M2)))
 
 
 
-# Novotny non-paraxial method
+# Novotny-Hetch non-paraxial method (Novotny, L., & Hecht, B. (2012). Principles of nano-optics. Cambridge university press.)
 
 
 
-def _cart_cyl_nov(x,y):
+def _cart_cyl_NH(x,y):
     rho = np.sqrt(x**2 + y**2)
     phi = np.arctan2(y,x)
     return rho,phi 
@@ -71,7 +71,7 @@ def _I11_plus_3xI12(x,y,z,k,n,NA,f,w0):  #Iazm=I11+3*I12
 
     thetamax = np.arcsin(NA/n)
     f0 = w0/(f*np.sin(thetamax))     
-    rho, phi = _cart_cyl_nov(x,y)    
+    rho, phi = _cart_cyl_NH(x,y)    
   
     real = integrate.quad(lambda theta: np.real(_fw(theta,f0,thetamax)*np.sqrt(np.cos(theta))
                          *np.sin(theta)**2*special.j1(k*rho*np.sin(theta))*np.exp(1j*k*z*np.cos(theta))), 0, thetamax)
@@ -80,20 +80,20 @@ def _I11_plus_3xI12(x,y,z,k,n,NA,f,w0):  #Iazm=I11+3*I12
     
     return real[0] + 1j*imag[0] 
 
-def PVB_nov_x(x,y,z,k,n,NA,f,w0):
+def PVB_NH_x(x,y,z,k,n,NA,f,w0):
     cn1=1
     cn2=1
     prefactor = 0.5*1j*k*f**2*np.sqrt(cn1/cn2)*np.exp(-1j*k*f)/w0
-    rho, phi = _cart_cyl_nov(x,y)
+    rho, phi = _cart_cyl_NH(x,y)
     I_azm = _I11_plus_3xI12(x,y,z,k,n,NA,f,w0)
     azi_x=prefactor * ( 1j*(I_azm)*np.sin(phi) )
     return azi_x
 
-def PVB_nov_y(x,y,z,k,n,NA,f,w0):
+def PVB_NH_y(x,y,z,k,n,NA,f,w0):
     cn1=1
     cn2=1
     prefactor = 0.5*1j*k*f**2*np.sqrt(cn1/cn2)*np.exp(-1j*k*f)/w0
-    rho, phi = _cart_cyl_nov(x,y)
+    rho, phi = _cart_cyl_NH(x,y)
     I_azm = _I11_plus_3xI12(x,y,z,k,n,NA,f,w0)
     azi_y=prefactor * ( -1j*(I_azm)*np.cos(phi) )
     return azi_y  
@@ -103,7 +103,7 @@ def I00(x,y,z,fcen,n,NA,f,w0):
     k=2*n*np.pi*fcen
     thetamax = np.arcsin(NA/n)
     f0 = w0/(f*np.sin(thetamax)) 
-    rho, phi = _cart_cyl_nov(x,y)    
+    rho, phi = _cart_cyl_NH(x,y)    
     real = integrate.quad(lambda theta: np.real(_fw(theta,f0,thetamax)*np.sqrt(np.cos(theta))
                           *np.sin(theta)*(1+np.cos(theta))*special.j0(k*rho*np.sin(theta))*np.exp(1j*k*z*np.cos(theta))), 0, thetamax)
     imag = integrate.quad(lambda theta: np.imag(_fw(theta,f0,thetamax)*np.sqrt(np.cos(theta))
@@ -116,7 +116,7 @@ def I01(x,y,z,fcen,n,NA,f,w0):
     k=2*n*np.pi*fcen
     thetamax = np.arcsin(NA/n)
     f0 = w0/(f*np.sin(thetamax))    
-    rho, phi = _cart_cyl_nov(x,y)    
+    rho, phi = _cart_cyl_NH(x,y)    
     real = integrate.quad(lambda theta: np.real(_fw(theta,f0,thetamax)*np.sqrt(np.cos(theta))
                           *np.sin(theta)**2*special.j1(k*rho*np.sin(theta))*np.exp(1j*k*z*np.cos(theta))), 0, thetamax)
     imag = integrate.quad(lambda theta: np.imag(_fw(theta,f0,thetamax)*np.sqrt(np.cos(theta))
@@ -129,7 +129,7 @@ def I02(x,y,z,fcen,n,NA,f,w0):
     k=2*n*np.pi*fcen
     thetamax = np.arcsin(NA/n)
     f0 = w0/(f*np.sin(thetamax))
-    rho, phi = _cart_cyl_nov(x,y)    
+    rho, phi = _cart_cyl_NH(x,y)    
     real = integrate.quad(lambda theta: np.real(_fw(theta,f0,thetamax)*np.sqrt(np.cos(theta))
                           *np.sin(theta)*(1-np.cos(theta))*special.jv(2,k*rho*np.sin(theta))*np.exp(1j*k*z*np.cos(theta))), 0, thetamax)
     imag = integrate.quad(lambda theta: np.imag(_fw(theta,f0,thetamax)*np.sqrt(np.cos(theta))
@@ -143,7 +143,7 @@ def I10(x,y,z,fcen,n,NA,f,w0):
     k=2*n*np.pi*fcen
     thetamax = np.arcsin(NA/n)
     f0 = w0/(f*np.sin(thetamax))    
-    rho, phi = _cart_cyl_nov(x,y)    
+    rho, phi = _cart_cyl_NH(x,y)    
     real = integrate.quad(lambda theta: np.real(_fw(theta,f0,thetamax)*np.sqrt(np.cos(theta))
                           *np.sin(theta)**3*special.j0(k*rho*np.sin(theta))*np.exp(1j*k*z*np.cos(theta))), 0, thetamax)
     imag = integrate.quad(lambda theta: np.imag(_fw(theta,f0,thetamax)*np.sqrt(np.cos(theta))
@@ -157,7 +157,7 @@ def I11(x,y,z,fcen,n,NA,f,w0):
     k=2*n*np.pi*fcen
     thetamax = np.arcsin(NA/n)
     f0 = w0/(f*np.sin(thetamax))     
-    rho, phi = _cart_cyl_nov(x,y)    
+    rho, phi = _cart_cyl_NH(x,y)    
     real = integrate.quad(lambda theta: np.real(_fw(theta,f0,thetamax)*np.sqrt(np.cos(theta))
                           *np.sin(theta)**2*(1+3*np.cos(theta))*special.j1(k*rho*np.sin(theta))*np.exp(1j*k*z*np.cos(theta))), 0, thetamax)
     imag = integrate.quad(lambda theta: np.imag(_fw(theta,f0,thetamax)*np.sqrt(np.cos(theta))
@@ -170,7 +170,7 @@ def I12(x,y,z,k,fcen,n,NA,f,w0):
     k=2*n*np.pi*fcen
     thetamax = np.arcsin(NA/n)
     f0 = w0/(f*np.sin(thetamax))     
-    rho, phi = _cart_cyl_nov(x,y)    
+    rho, phi = _cart_cyl_NH(x,y)    
     real = integrate.quad(lambda theta: np.real(_fw(theta,f0,thetamax)*np.sqrt(np.cos(theta))
                           *np.sin(theta)**2*(1-np.cos(theta))*special.j1(k*rho*np.sin(theta))*np.exp(1j*k*z*np.cos(theta))), 0, thetamax)
     imag = integrate.quad(lambda theta: np.imag(_fw(theta,f0,thetamax)*np.sqrt(np.cos(theta))
@@ -185,7 +185,7 @@ def Ex_H00(x,y,z,fcen,n,NA,f,w0):
     cn1=1
     cn2=1
     prefactor = 0.5*1j*k*f**2*np.sqrt(cn1/cn2)*np.exp(-1j*k*f)/w0
-    rho, phi = _cart_cyl_nov(x,y)
+    rho, phi = _cart_cyl_NH(x,y)
     I_00 = I00(x,y,z,k,n,NA,f,w0)
     I_02 = I02(x,y,z,k,n,NA,f,w0)
     H00_x = prefactor * I_00 + I_02*np.cos(2*phi)
@@ -196,7 +196,7 @@ def Ey_H00(x,y,z,fcen,n,NA,f,w0):
     cn1=1
     cn2=1
     prefactor = 0.5*1j*k*f**2*np.sqrt(cn1/cn2)*np.exp(-1j*k*f)/w0
-    rho, phi = _cart_cyl_nov(x,y)
+    rho, phi = _cart_cyl_NH(x,y)
     I_02 = I02(x,y,z,k,n,NA,f,w0)
     H00_y = prefactor * I_02 * np.sin(2*phi)
     return H00_y
@@ -206,7 +206,7 @@ def Ez_H00(x,y,z,fcen,n,NA,f,w0):
     cn1=1
     cn2=1
     prefactor = 0.5*1j*k*f**2*np.sqrt(cn1/cn2)*np.exp(-1j*k*f)/w0
-    rho, phi = _cart_cyl_nov(x,y)
+    rho, phi = _cart_cyl_NH(x,y)
     I_01 = I01(x,y,z,k,n,NA,f,w0)
     H00_z = prefactor * (-2 * 1j * I_01 * np.cos(phi) )   
     return H00_z
